ed3914c7702f6d457eac2e93357c991db76cad63
Daniel OFarrell <ofarreld@onid.oregonstate.edu>
2015-05-10 17:38:14 +0900

    Began work with mysql connection problem at 3:20pm

--- assign0402j.js
+++ assign0402j.js
@@ -1,8 +1,8 @@
 // this is for js
 window.onload = function() {
-  localStorage.setItem('filterBy', 'none');
-  localStorage.setItem('filter', 'none');
-  localStorage.setItem('sortBy', 'none');
+ // localStorage.setItem('filterBy', 'none');
+ // localStorage.setItem('filter', 'none');
+ // localStorage.setItem('sortBy', 'name');
   makeRequest('action=init');
   
 };
@@ -15,26 +15,21 @@ function makeRequest (statement) {
     xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
   }
  
-  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-    var response = xmlhttp.responseText;
-    var elem = document.getElementById('outputTable');
-    elem.innerHTML = response;
-    localStorage.setItem('localPlayers', response );
-    // addListenerts();
-  }
-  /*
-  if (statement == 'action=add') {
-    
-  }
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      var response = xmlhttp.responseText;
+      var elem = document.getElementById('outputTable');
+      elem.innerHTML = response;
+      localStorage.setItem('movieTable', response );
+      // addListenerts();
+    }
+  }/*
   var filter = 'filter=' + localStorage.getItem('filter');
   var filterBy = 'filterBy=' + localStorage.getItem('filterBy');
   var sortBy = 'sortBy=' + localStorage.getItem('sortBy');
   statement += '&' + filter + '&' + filterBy + '&' + sortBy;
-  */
-  
-  xmlhttp.open("GET",'assign0402p.php?' + statement,true);
+    */
+  var url = "assign0402p.php?";
+  xmlhttp.open("GET",url + statement,true);
   xmlhttp.send();
-  
-  
-  
 }
--- assign0402p.php
+++ assign0402p.php
@@ -1,49 +1,70 @@
 <?php
 
 // include 'assign0402config.php';
-
-
-// id, name, category, length, rented
 $dbhost = 'oniddb.cws.oregonstate.edu';
 $dbuser = 'ofarreld-db';
 $dbpass = 'cfll9z41YEI1fBfb';
+$dbname = 'ofarreld-db';
 $table = 'cs290as0402movies';
 
+$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
-
-$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbuser);
 if (!$mysqli || $mysqli->connection_errno) {
   echo "Connection error" . $mysqli->connection_errno . " " . $mysqli->connect_error;
-} else {
-  echo "connection worked!";
+} 
+
+function init() {
+  global $table, $mysqli;
+  $all = $mysqli->prepare("SELECT * FROM $table");
+  $all->execute();
+  $res = $all->get_results();
+  while($row = $res->fetch_assoc()) {
+    echo $row['id'];
+  }
+
   
 }
 
+// sort the requests sent from javascript
+if(isset($_REQUEST['action'])) {
+  $action = $_REQUEST['action'];
+  if ($action == 'init') {
+    init();
+    
+    
+  }
+}
 
+
+
+/*
 function init() {
   global $mysqli, $table;
+
+  var_dump($mysqli);
+}
+  /*
   // initializing table data from the db
   $all = $mysqli->prepare("SELECT * FROM $table");
   $all->execute();
   $result = $all->get_result();
+  
+  echo "<br> Whatever <br>";
+  var_dump($result);
   
   buildTable($result);  
   $all->close(); 
 
 }
 
-if(isset($_REQUEST['action'])) {
-  $action = $_REQUEST['action'];
-}
-
 // id, name, category, length, rented
 function buildTable($res) {
   echo '<table>';
   echo '<tr>';
-  echo '<td>First Name</td>';
-  echo '<td>Last Name</td>';
-  echo '<td>Position</td>';
-  echo '<td>Team</td>';
+  echo '<td>Movie Title</td>';
+  echo '<td>Length</td>';
+  echo '<td>Category</td>';
+  echo '<td>Status</td>';
   echo '</tr>';
   while($row = $res->fetch_assoc())
   {
@@ -52,11 +73,9 @@ function buildTable($res) {
     echo '<td>'.$row['length'].'</td>';
     echo '<td class="position">'.$row['category'].'</td>';
     echo '<td>';
-    echo '<td class="update">Update</td>';
-    echo '<td class="remove">Remove</td>';
     echo '</tr>';
   }
   echo '</table>';  
-}
+}*/
 
 ?>
