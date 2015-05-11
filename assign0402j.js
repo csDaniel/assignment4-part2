b37067a31c499c37660304ae82c9b38e0083574d
Daniel OFarrell <ofarreld@onid.oregonstate.edu>
2015-05-11 14:48:17 +0900

    began at 11:30, finished accessing db, adding, and removing entries

--- assign0402j.js
+++ assign0402j.js
@@ -1,9 +1,11 @@
 // this is for js
 window.onload = function() {
- // localStorage.setItem('filterBy', 'none');
- // localStorage.setItem('filter', 'none');
- // localStorage.setItem('sortBy', 'name');
+  localStorage.setItem('filterBy', 'none');
+  localStorage.setItem('filter', 'none');
+  localStorage.setItem('sortBy', 'name');
+
   makeRequest('action=init');
+  document.getElementById('buttonInput').addEventListener('click', addMovie);
   
 };
 
@@ -20,16 +22,42 @@ function makeRequest (statement) {
       var response = xmlhttp.responseText;
       var elem = document.getElementById('outputTable');
       elem.innerHTML = response;
-      //localStorage.setItem('movieTable', response );
-      // addListenerts();
+      addListeners();
     }
-  }/*
+  }
+  if (statement == 'action=add') {
+    var elem = document.getElementById('addVideoForm');
+    statement += '&nameInput=' + elem.elements['nameInput'].value;
+    statement += '&lengthInput=' + elem.elements['lengthInput'].value;
+    statement += '&categoryInput=' + elem.elements['categoryInput'].value;  
+  }
+ 
   var filter = 'filter=' + localStorage.getItem('filter');
   var filterBy = 'filterBy=' + localStorage.getItem('filterBy');
   var sortBy = 'sortBy=' + localStorage.getItem('sortBy');
   statement += '&' + filter + '&' + filterBy + '&' + sortBy;
-    */
+    
   var url = "assign0402p.php?";
   xmlhttp.open("GET",url + statement,true);
   xmlhttp.send();
+}
+
+function addMovie() {
+  var statement = 'action=add';
+  makeRequest(statement);  
+}
+
+function removeVideo() {
+  // statement == command + id of entire movie
+  console.log(this.parentNode.parentNode.id);
+  var statement = 'action=remove&id=' + this.parentNode.parentNode.id;
+  makeRequest(statement);
+}
+
+function addListeners() {
+
+  var removes = document.getElementsByClassName("removeVid");
+  for (var i = 0; i < removes.length; i++) {
+    removes[i].addEventListener('click', removeVideo);    
+  }
 }
--- assign0402p.php
+++ assign0402p.php
@@ -1,67 +1,109 @@
 <?php
 include 'assign0402config.php';
 
+ini_set('display_errors', 'On');
 
+
+// id, name, category, length, rented
 $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-
 if (!$mysqli || $mysqli->connection_errno) {
-  echo "Connection error" . $mysqli->connection_errno . " " . $mysqli->connect_error;
+  echo "Failed to conbnect to MySQL: (" . $mysqli->connection_errno . ") " . $mysqli->connect_error;
+} else {
+  echo "Current Inventory:";
 }
 
 
 function init() {
- /* global $table;
+  global $table;
   global $mysqli;  
  
   // initializing table data from the db
   $all = $mysqli->prepare("SELECT * FROM $table");
   $all->execute();
-  $result = $all->get_result();
-
- // buildTable($result);  
-  $all->close(); 
-}*/
-
-  global $mysqli, $table;
-  $all = $mysqli->prepare("SELECT * FROM $table");
-  $all->execute();
   $res = $all->get_result();
-  //  $list = array();
-  buildTable($res);
-  //echo json_encode($list);
-  $all->close();
+  
+ 
+  makeTable($res);
+  
+  $res->close();   
+}
+
+function makeTable($results) {
+  echo '<table>';
+  echo '<tr>';
+  echo '<td>Title</td>';
+  echo '<td>Category</td>';
+  echo '<td>Length</td>';
+  echo '<td>Status</td>';
+  echo '</tr>';
+  while ($row = $results->fetch_assoc()) {
+    echo '<tr id="'.$row['id'].'">';
+    echo '<td>' .$row['id'].'</td>';
+    echo '<td>' .$row['name']. '</td>';
+    echo '<td>' .$row['category']. '</td>';    
+    echo '<td>' .$row['length']. '</td>';   
+    echo '<td> <div class="status">Rental Status</div></td>';
+    echo '<td> <div class="removeVid">Remove</div></td>';
+    echo '</tr>';
+    
+  }
+  echo '</table>';
+}
+
+function sortTable() {
+  global $mysqli;
+  global $table;
+  
+  $revised = $mysqli->prepare("SELECT * FROM $table");
+  $revised->execute();
+  $res = $revised->get_result();
+  
+  makeTable($res);
+  
+  $res->close();
+  
+  
+}
+
+function addMovie($nName, $nLength, $nCat) {
+  // nameInput, lengthInput, categoryInput, 
+  global $mysqli;
+  global $table;
+  $rented = 0;
+  $add = $mysqli->prepare("INSERT INTO $table (name, category, length, rented) VALUES (?,?,?,?)");
+  $add->bind_param('ssii', $nName, $nCat, $nLength, $rented);
+  $add->execute();
+  $add->close();
+}
+
+function removeMovie($id) {
+  global $mysqli;
+  global $table;
+  $remove = $mysqli->prepare("DELETE from $table WHERE id = ?");
+  $remove->bind_param('i', $id);
+  $remove->execute();
+  $remove->close();
+  
 }
   
 // sort the requests sent from javascript
 if(isset($_REQUEST['action'])) {
   $action = $_REQUEST['action'];
   if ($action == 'init') {
-    init();
+    //init();
+  } else if ($action == 'add') {
+    $nName = $_REQUEST['nameInput'];
+    $nLength = $_REQUEST['lengthInput'];
+    $nCat = $_REQUEST['categoryInput'];
+    addMovie($nName, $nLength, $nCat);
+  } else if ($action == 'remove') {
+    $id = $_REQUEST['id'];
+    echo $id;
+    removeMovie($id);
   }
+  // default case. currently defaults when action=init
+  sortTable();
 }
 
-
-
-// id, name, category, length, rented
-function buildTable($res) {
-  echo '<table>';
-  echo '<tr>';
-  echo '<td>Movie Title</td>';
-  echo '<td>Length</td>';
-  echo '<td>Category</td>';
-  echo '<td>Status</td>';
-  echo '</tr>';
-  while($row = $res->fetch_assoc())
-  {
-    echo '<tr id="'.$row['id'].'">';
-    echo '<td>'.$row['name'].'</td>';
-    echo '<td>'.$row['length'].'</td>';
-    echo '<td>'.$row['category'].'</td>';
-    echo '<td> <div id="rented'.$row['id'].'>Rented</div></td>';
-    echo '<td>';
-    echo '</tr>';
-  }
-  echo '</table>';  
-} 
 
 ?>
